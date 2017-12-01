c157be88bc2925b2090ae6fcc483cd595fbc4e5b
StatusCode200 <358941501@qq.com>
2017-12-01 13:01:06 +0800

    一点小更改

---
+++ src/main/java/com/xmu/ooad/classmanagementsystem/controller/UploadController.java
@@ -0,0 +1,22 @@
+package com.xmu.ooad.classmanagementsystem.controller;
+
+import com.xmu.ooad.classmanagementsystem.vo.UrlVO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author status200
+ * @date 2017/11/30
+ */
+@RestController
+public class UploadController {
+
+    @PostMapping("/upload/avatar")
+    public ResponseEntity<UrlVO> updateAvatar() {
+
+        return new ResponseEntity<>(new UrlVO("/avatar/3486.png"), HttpStatus.CREATED);
+    }
+}

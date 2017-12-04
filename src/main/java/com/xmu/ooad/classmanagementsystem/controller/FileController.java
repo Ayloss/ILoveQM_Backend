fe612d723e58c97fc2dce72598609f622c83f43a
StatusCode200 <358941501@qq.com>
2017-12-05 00:21:16 +0800

    新增头像url /avatar/{filename}

--- src/main/java/com/xmu/ooad/classmanagementsystem/controller/FileController.java
+++ src/main/java/com/xmu/ooad/classmanagementsystem/controller/FileController.java
@@ -1,11 +1,21 @@
 package com.xmu.ooad.classmanagementsystem.controller;
 
-import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.imageio.stream.ImageInputStream;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
@@ -16,10 +26,16 @@ import java.io.IOException;
 @Controller
 public class FileController {
 
+
     @GetMapping(value = "/avatar/{fileName}")
-    public void getAvatar(HttpServletResponse response) throws IOException {
+    public ResponseEntity<byte[]> getAvatar(HttpServletResponse response) throws IOException {
         response.setContentType(MediaType.IMAGE_PNG_VALUE);
 
-//        IOUtils
+        File file = ResourceUtils.getFile("classpath:avatar.png");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_PNG);
+
+        return new ResponseEntity<>(FileUtils.readFileToByteArray(file), headers, HttpStatus.OK);
     }
 }

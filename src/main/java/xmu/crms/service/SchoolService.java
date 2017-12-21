2649d843b5a300fc13c47bf7b3cb96da89134d3b
noName10 <xolinxmu@gmail.com>
2017-12-21 20:44:06 +0800

    test

--- src/main/java/xmu/crms/service/SchoolService.java
+++ src/main/java/xmu/crms/service/SchoolService.java
@@ -31,23 +31,6 @@ public interface SchoolService {
 	 BigInteger insertSchool(School school);
 	
 	/**
-	 * 获取省份列表.
-	 * <p>获取所有省份的名称<br>
-	 * @author LiuAiqi
-	 * @return list 省份名称列表
-	 */
-	 List<String> listProvince();
-	
-	/**
-	 * 获取城市列表.
-	 * <p>根据省份名称，获取该省份所有城市名称，返回城市名称列表<br>
-	 * @author LiuAiqi
-	 * @param province 省份名称
-	 * @return list 城市名称列表
-	 */
-	 List<String> listCity(String province);
-	
-	/**
 	 * 获取学校信息.
 	 * <p>根据学校id，获取该学校的所有信息<br>
 	 * @author LiuAiqi
